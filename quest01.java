0ed71c42d0604684331026c354097e2ac2c78547
Liah Souza <115299056+thalia-araujoo@users.noreply.github.com>
2023-10-04 11:30:54 -0300

    Add files via upload

---
+++ quest01.java
@@ -0,0 +1,15 @@
+class ImprimirNumeros {
+    
+    public static void main(String[] args) {
+        // Início do intervalo
+        int inicio = 150;
+        
+        // Fim do intervalo
+        int fim = 300;
+        
+        // Loop para imprimir os números de 150 a 300
+        for (int i = inicio; i <= fim; i++) {
+            System.out.println(i);
+        }
+    }
+}
