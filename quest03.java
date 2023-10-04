a979f01510749c77a468fbd0b1c18f7516c184b8
Liah Souza <115299056+thalia-araujoo@users.noreply.github.com>
2023-10-04 11:49:34 -0300

    Add files via upload

---
+++ quest03.java
@@ -0,0 +1,11 @@
+ class MultiplosDe3 {
+    public static void main(String[] args) {
+        System.out.println("Múltiplos de 3 entre 1 e 100:");
+        
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
