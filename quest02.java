0983d843c2e4f67bd6635fbf9709b3f4493fd6bc
Liah Souza <115299056+thalia-araujoo@users.noreply.github.com>
2023-10-04 11:42:02 -0300

    Add files via upload

---
+++ quest02.java
@@ -0,0 +1,12 @@
+class SomaDe1Ate1000 {
+    public static void main(String[] args) {
+        int soma = 0;
+        
+        for (int i = 1; i <= 1000; i++) {
+            soma += i;
+        }
+        
+        System.out.println("A soma de 1 a 1000 é: " + soma);
+    }
+}
+
